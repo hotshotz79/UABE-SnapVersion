9c1206015589cb383ae634666a8529065b2ba096
hotshotz79 <hotshotz79@hotmail.com>
2023-09-10 14:32:10 -0400

    Add files via upload

---
+++ AssetContainer.cs
@@ -0,0 +1,110 @@
+﻿using AssetsTools.NET;
+using AssetsTools.NET.Extra;
+using System;
+using System.IO;
+
+namespace UABEAvalonia
+{
+    // improvement over AssetExternal to handle dynamic changes
+    // not to be confused with unity containers which are just
+    // unity's way of showing what export an asset is connected to
+    public class AssetContainer
+    {
+        public long PathId { get; }
+        public int ClassId { get; }
+        public ushort MonoId { get; }
+        public uint Size { get; }
+        public string Container { get; set; } // should be a list later
+        public AssetsFileInstance FileInstance { get; }
+        public AssetTypeValueField? BaseValueField { get; }
+
+        public long FilePosition { get; private set; }
+        public AssetsFileReader? FileReader { get; private set; }
+
+        // deprecated
+        public AssetID AssetId
+        {
+            get => new AssetID(FileInstance.path, PathId);
+        }
+        public AssetPPtr AssetPPtr
+        {
+            get => new AssetPPtr(FileInstance.path, 0, PathId);
+        }
+        public bool HasValueField
+        {
+            get => BaseValueField != null;
+        }
+
+        // existing assets
+        public AssetContainer(AssetFileInfo info, AssetsFileInstance fileInst, AssetTypeValueField? baseField = null)
+        {
+            FilePosition = info.AbsoluteByteStart;
+            FileReader = fileInst.file.Reader;
+
+            PathId = info.PathId;
+            ClassId = info.TypeId;
+            MonoId = fileInst.file.GetScriptIndex(info);
+            Size = info.ByteSize;
+            Container = string.Empty;
+            FileInstance = fileInst;
+            BaseValueField = baseField;
+        }
+
+        // newly created assets
+        public AssetContainer(AssetsFileReader fileReader, long assetPosition, long pathId, int classId, ushort monoId, uint size,
+                              AssetsFileInstance fileInst, AssetTypeValueField? baseField = null)
+        {
+            FilePosition = assetPosition;
+            FileReader = fileReader;
+
+            PathId = pathId;
+            ClassId = classId;
+            MonoId = monoId;
+            Size = size;
+            Container = string.Empty;
+            FileInstance = fileInst;
+            BaseValueField = baseField;
+        }
+
+        // modified assets
+        public AssetContainer(AssetContainer container, AssetsFileReader fileReader, long assetPosition, uint size)
+        {
+            FilePosition = assetPosition;
+            FileReader = fileReader;
+
+            PathId = container.PathId;
+            ClassId = container.ClassId;
+            MonoId = container.MonoId;
+            Size = size;
+            Container = string.Empty;
+            FileInstance = container.FileInstance;
+            BaseValueField = container.BaseValueField;
+        }
+
+        public AssetContainer(AssetContainer container, AssetTypeValueField baseField)
+        {
+            FilePosition = container.FilePosition;
+            FileReader = container.FileReader;
+
+            PathId = container.PathId;
+            ClassId = container.ClassId;
+            MonoId = container.MonoId;
+            Size = container.Size;
+            Container = string.Empty;
+            FileInstance = container.FileInstance;
+            BaseValueField = baseField;
+        }
+
+        public void SetNewFile(AssetsFileInstance fileInst)
+        {
+            AssetFileInfo info = fileInst.file.GetAssetInfo(PathId);
+            if (info == null)
+            {
+                throw new Exception("Missed an asset during save. Path ID: " + PathId);
+            }
+
+            FilePosition = info.AbsoluteByteStart;
+            FileReader = fileInst.file.Reader;
+        }
+    }
+}
---
+++ PluginWindow.axaml.cs
@@ -0,0 +1,73 @@
+using AssetsTools.NET.Extra;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using AvaloniaEdit;
+using System;
+using System.Collections.Generic;
+using UABEAvalonia.Plugins;
+
+namespace UABEAvalonia
+{
+    public partial class PluginWindow : Window
+    {
+        private Window win;
+        private AssetWorkspace workspace;
+        private List<AssetContainer> selection;
+
+        List<UABEAPluginMenuInfo> plugInfs;
+
+        public PluginWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            //generated events
+            btnOk.Click += BtnOk_Click;
+            btnCancel.Click += BtnCancel_Click;
+        }
+
+        public PluginWindow(Window win, AssetWorkspace workspace, List<AssetContainer> selection, PluginManager plugLoader) : this()
+        {
+            this.win = win;
+            this.workspace = workspace;
+            this.selection = selection;
+
+            plugInfs = plugLoader.GetPluginsThatSupport(workspace.am, selection);
+            boxPluginList.Items = plugInfs;
+            //Auto Select "Edit Texture" (index 1) and click OK
+            boxPluginList.SelectedIndex = 1;
+            btnOk.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private async void BtnOk_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var menuPlugInf = boxPluginList.SelectedItem as UABEAPluginMenuInfo;
+
+            if (menuPlugInf == null)
+            {
+                Close(false);
+                return;
+            }
+
+            var plugOpt = menuPlugInf.pluginOpt;
+            try
+            {
+                await plugOpt.ExecutePlugin(win, workspace, selection);
+            }
+            catch (Exception ex)
+            {
+                await MessageBoxUtil.ShowDialog(this, "Plugin Exception!", $"Plugin {menuPlugInf.displayName} has crashed. Stacktrace:\n" + ex.ToString());
+            }
+            Close(true);
+        }
+
+        private void BtnCancel_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            Close(false);
+        }
+    }
+}
